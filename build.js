997dc0002dd9b28d494b6c53be145e93a34e098e
Julien <julienauvray28@gmail.com>
2024-02-24 00:39:11 +0100

    chore: change initial setup  remove jest  add japa  use esbuild  move in ESM

---
+++ build.js
@@ -0,0 +1,36 @@
+import { build } from "esbuild";
+import { dtsPlugin } from "esbuild-plugin-d.ts";
+
+const option = {
+  bundle: true,
+  color: true,
+  logLevel: "info",
+  sourcemap: true,
+  platform: "node",
+  entryPoints: ["./src/index.ts"],
+  minify: true,
+};
+
+async function run() {
+  await build({
+    format: "esm",
+    outdir: "./dist",
+    splitting: true,
+    plugins: [dtsPlugin()],
+    ...option,
+  }).catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
+
+  await build({
+    format: "cjs",
+    outfile: "./dist/index.cjs.js",
+    ...option,
+  }).catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
+}
+
+run();
--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    roots: ['<rootDir>/src'],
-    testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
-    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-}
