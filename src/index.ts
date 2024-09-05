9ba1e08d59508e5a1426cdb081f044a1c8e21aca
Julien <julienauvray28@gmail.com>
2024-09-05 10:31:41 +0200

    refactor: Remove unused code and optimize imports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import type { GameEngine } from "./core/portUserside/engine.js";
 import { configureEngine, type EngineConfig } from "./config.js";
-import { isGameWithNextAction } from "./core/domain/types/game.js";
 
 export const createGameEngine = (config: Partial<EngineConfig>): GameEngine => {
   const {
@@ -29,36 +28,3 @@ export const createGameEngine = (config: Partial<EngineConfig>): GameEngine => {
     getResults: (command) => getResultHandler(command),
   };
 };
-
-const engine = createGameEngine({});
-
-const a = engine.createGame({ mode: "standard" });
-const b = engine.getModes({});
-const c = engine.getExtraRules({ mode: "standard", players: 2 });
-const d = engine.addPlayers({ game: a, players: ["player1"] });
-const e = engine.addExtraRules({ game: d, extraRules: ["rule1"] });
-const f = engine.startGame({ game: e });
-const g = engine.chooseDomino({
-  game: f,
-  lordId: "player1",
-  dominoPick: 1,
-});
-const h = engine.discardDomino({ game: g, lordId: "player1" });
-const i = engine.placeDomino({
-  game: g,
-  lordId: "player1",
-  position: { x: 1, y: 1 },
-  orientation: "horizontal",
-  rotation: 0,
-});
-if (isGameWithNextAction(i)) {
-  const j = engine.chooseDomino({
-    game: i,
-    lordId: "player1",
-    dominoPick: 1,
-  });
-}
-
-if (!isGameWithNextAction(i)) {
-  const j = engine.getResults({ game: i });
-}
